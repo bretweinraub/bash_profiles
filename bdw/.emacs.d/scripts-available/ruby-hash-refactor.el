c306bbb3e718794a242c6c817becb6996243101f
Bret Weinraub <bret@aura-software.com>
2017-12-07 12:56:01 +0100

    add in ruby-hash-refactor

---
+++ bdw/.emacs.d/scripts-available/ruby-hash-refactor.el
@@ -0,0 +1,25 @@
+
+
+(defun refactor-hash ()
+  "convert old style ruby hash to new style ruby hash"
+  (interactive)
+  (search-backward ":")
+  (delete-char 1)
+  (search-forward-regexp " ")
+  (backward-char 1)
+  (insert ":")
+  (delete-char 3)
+  )
+
+
+(defun my-ehr-mode-config ()
+  "For use in `html-mode-hook'."
+  (local-set-key (kbd "C-c C-r h") (symbol-function 'refactor-hash))  
+  )
+
+;; add to hook
+(add-hook 'ehr-mode-hook 'my-ehr-mode-config)
+
+
+
+;; foo: :bar
